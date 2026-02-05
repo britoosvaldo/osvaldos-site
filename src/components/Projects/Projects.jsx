7b7d3e0e23b018fbef582977717c18fe0911cb2f
Osvaldobrito <osvaldooabn@gmail.com>
2026-02-05 17:41:22 -0400

    Atualização dos cards do projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,7 +6,26 @@ export default function About() {
         Trabalhos <span>recentes</span>
       </h2>
       <div className="projects__grid">
-        <div className="projects__card"></div>
+        <div className="projects__card">
+          <img className="site__icon" src="src/images/users-icon.svg" />
+          <a href="https://github.com/britoosvaldo/web_project_api_full">
+            <img
+              className="github__icon"
+              src="src/images/github-icon-grey.svg"
+            />
+          </a>
+          <a href="https://around-the-usa.mooo.com">
+            <img className="link__icon" src="src/images/link-icon-grey.svg" />
+          </a>
+          <h3 className="site__title">Around the USA</h3>
+          <p className="site__description">
+            Rede Social que permite aos usuários compartilhar fotos, editar seu
+            perfil, curtir e deletar publicações. (TripleTen)
+          </p>
+          <p className="site__techs">
+            HTML / CSS / JavaScript / React / Node.js / MongoDB
+          </p>
+        </div>
         <div className="projects__card"></div>
         <div className="projects__card"></div>
         <div className="projects__card"></div>
