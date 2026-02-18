c2199d603e33ee3da947d792d6dbe782f5533522
Osvaldobrito <osvaldooabn@gmail.com>
2026-02-18 12:56:37 -0400

    Criação do burguer do header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,60 @@
+import { useState } from "react";
 import obLogo from "../../images/ob-logo.svg";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="header">
-      <nav className="nav">
-        <img className="nav__logo" src={obLogo} />
+      <nav className={`nav ${isOpen ? "nav--open" : ""}`}>
+        <img className="nav__logo" src={obLogo} alt="Logo" />
+
+        <button
+          type="button"
+          className="nav__toggle"
+          onClick={() => setIsOpen((prev) => !prev)}
+          aria-expanded={isOpen}
+          aria-label={isOpen ? "Fechar menu" : "Abrir menu"}
+        >
+          <span className="nav__bar" />
+          <span className="nav__bar" />
+          <span className="nav__bar" />
+        </button>
+
         <ul className="nav__links">
           <li>
-            <a className="nav__link" href="#about">
+            <a
+              className="nav__link"
+              href="#about"
+              onClick={() => setIsOpen(false)}
+            >
               Início
             </a>
           </li>
           <li>
-            <a className="nav__link" href="#about">
+            <a
+              className="nav__link"
+              href="#about"
+              onClick={() => setIsOpen(false)}
+            >
               Sobre
             </a>
           </li>
           <li>
-            <a className="nav__link" href="#projects">
+            <a
+              className="nav__link"
+              href="#projects"
+              onClick={() => setIsOpen(false)}
+            >
               Projetos
             </a>
           </li>
           <li>
-            <a className="nav__link" href="#contacts">
+            <a
+              className="nav__link"
+              href="#contacts"
+              onClick={() => setIsOpen(false)}
+            >
               Contatos
             </a>
           </li>
