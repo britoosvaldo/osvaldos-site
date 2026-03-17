ca5567e0ebecc8dc70d1b1c268f06b8263adf181
Osvaldobrito <osvaldooabn@gmail.com>
2026-03-17 17:59:36 -0400

    Separação de arquivos data de projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,74 +1,34 @@
-import gitIcon from "../../images/github-icon-grey.svg";
-import siteIcon from "../../images/link-icon-grey.svg";
-import usersIcon from "../../images/users-icon.svg";
-import newsPaperIcon from "../../images/newspaper-icon.svg";
-import gitIconOrange from "../../images/github-icon-orange.svg";
-import siteIconOrange from "../../images/link-icon-orange.svg";
-
 import HoverIcon from "../HoverIcons/HoverIcons";
+import gitIconOrange from "../../images/github-icon-orange.svg";
+import siteIcon from "../../images/link-icon-grey.svg";
+import siteIconOrange from "../../images/link-icon-orange.svg";
+import { projectsCardsData } from "../../data/projectsCardsData";
 
 export default function Projects() {
   return (
     <section className="projects" id="projects">
       <p className="my-projects reveal">// meus projetos</p>
-
       <h2 className="projects__title reveal">
         Trabalhos <span>recentes</span>
       </h2>
 
-      {/* 👇 Reveal group aqui */}
       <div className="projects__grid reveal-group">
-        <div className="projects__card reveal">
-          <img className="card__icon" src={usersIcon} />
-          <a
-            className="github__link"
-            href="https://github.com/britoosvaldo/web_project_api_full"
-          >
-            <HoverIcon
-              firstColor={gitIcon}
-              secondColor={gitIconOrange}
-              alt="GitHub"
-            />
-          </a>
+        {projectsCardsData.map((project) => (
+          <div className="projects__card reveal" key={project.id}>
+            <img className="card__icon" src={project.icon} />
+            <a className="github__link" href={project.gitHubLink}>
+              <HoverIcon
+                firstColor={project.linkIcon}
+                secondColor={gitIconOrange}
+                alt="GitHub"
+              />
+            </a>
 
-          <h3 className="site__title">Around the USA</h3>
-          <p className="site__description">
-            Rede Social que permite aos usuários compartilhar fotos, editar seu
-            perfil, curtir e deletar publicações. (TripleTen)
-          </p>
-          <p className="site__techs">
-            HTML / CSS / JavaScript / React / Node.js / MongoDB
-          </p>
-        </div>
-
-        <div className="projects__card reveal">
-          <img className="card__icon" src={newsPaperIcon} />
-          <a
-            className="github__link"
-            href="https://github.com/britoosvaldo/web-news-frontend"
-          >
-            <HoverIcon
-              firstColor={gitIcon}
-              secondColor={gitIconOrange}
-              alt="GitHub"
-            />
-          </a>
-
-          <h3 className="site__title">Web News</h3>
-          <p className="site__description">
-            Site de pesquisa de notícias, onde o usuário, após ter feito
-            registro e login, pode salvar em sua conta as notícias de seu
-            interesse. (TripleTen)
-          </p>
-          <p className="site__techs">
-            HTML / CSS / JavaScript / React / Node.js / MongoDB
-          </p>
-        </div>
-
-        <div className="projects__card reveal"></div>
-        <div className="projects__card reveal"></div>
-        <div className="projects__card reveal"></div>
-        <div className="projects__card reveal"></div>
+            <h3 className="site__title">{project.title}</h3>
+            <p className="site__description">{project.description}</p>
+            <p className="site__techs">{project.skills}</p>
+          </div>
+        ))}
       </div>
 
       <div className="projects__link reveal">
---
+++ src/data/projectsCardsData.js
@@ -0,0 +1,37 @@
+import gitIcon from "../images/github-icon.svg";
+import usersIcon from "../images/users-icon.svg";
+import helmetIcon from "../images/helmet-icon.svg";
+import newsIcon from "../images/newspaper-icon.svg";
+
+export const projectsCardsData = [
+  {
+    id: 1,
+    icon: helmetIcon,
+    linkIcon: gitIcon,
+    gitHubLink: "https://github.com/britoosvaldo/project-mp",
+    title: "MP Engenharia",
+    description:
+      "Site de uma empresa de engenharia do estado de Roraima, (Em andamento)",
+    skills: "HTML / CSS / JavaScript / React",
+  },
+  {
+    id: 2,
+    icon: usersIcon,
+    linkIcon: gitIcon,
+    gitHubLink: "https://github.com/britoosvaldo/web_project_api_full",
+    title: "Around the USA",
+    description:
+      "Rede Social que permite aos usuários compartilhar fotos, editar seu perfil, curtir e deletar publicações. (TripleTen)",
+    skills: "HTML / CSS / JavaScript / React / Node.js / MongoDB",
+  },
+  {
+    id: 3,
+    icon: newsIcon,
+    linkIcon: gitIcon,
+    gitHubLink: "https://github.com/britoosvaldo/web-news-frontend",
+    title: "Web News",
+    description:
+      "Site de pesquisa de notícias, onde o usuário pode salvar em sua conta as notícias de seu interesse. (TripleTen)",
+    skills: "HTML / CSS / JavaScript / React / Node.js / MongoDB",
+  },
+];
